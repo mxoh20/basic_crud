d5cbc37932696199fcb66586216b88122d9aab2b
mxoh <mxoh8.m@gmail.com>
2019-09-23 15:29:37 +0200

    edit navigation

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -16,8 +16,6 @@
             <ul>
                 <li><a href="{{url('devices')}}">Home</a></li>
                 <li><a href="{{url('devices/create')}}">Add</a></li>
-                <li><a href="#">Components</a></li>
-                <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
             </ul>
         </nav>
 
